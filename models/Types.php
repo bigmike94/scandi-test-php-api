5fdc3a30ac46cd23eafac13a031498cd64b72b72
bigmike94 <misha.kirichenko.2994@gmail.com>
2021-12-15 21:34:41 +0400

    update Products and Types models

--- models/Types.php
+++ models/Types.php
@@ -1,8 +1,16 @@
 <?php
-header('Access-Control-Allow-Origin: *');//resources allowed
+// //local environment
+header('Access-Control-Allow-Origin: *'); 
 header('Content-Type: application/json');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods');
 header('Access-Control-Allow-Methods: GET');
+
+//for production
+// header('Access-Control-Allow-Origin: https://scandi-app.000webhostapp.com/');//resources allowed
+// header('Access-Control-Allow-Headers: Origin, Content-type');
+// header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
+// header('Content-Type: application/json');
+
 class Types extends Request
 {
 	private $types;
